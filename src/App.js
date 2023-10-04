89bf8bfda0913c2de2be1889e38b2b71a20a548d
RzWilliam <williamrodriguez0621@gmail.com>
2023-10-04 13:45:18 +0200

    Add SCSS, Tailwind, Prettier, Husky, Test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.scss'
-import FetchData from './components/DataFetch';
 
 function App() {
   return (
     <>
-      <FetchData url=""/>
       <div className='text-3xl font-bold underline'>Hello WORLD</div>
     </>
   );
--- src/components/DataFetch.test.js
+++ src/components/DataFetch.test.js
@@ -12,13 +12,10 @@ describe('DataFetch', () => {
 
     const { findByText } = render(<DataFetch url="http://example.com/api" />);
 
-    // Vérifie que fetchData est appelée
     expect(fetch).toHaveBeenCalledWith('http://example.com/api');
 
-    // Attends que l'effet secondaire soit exécuté
     const dataElement = await screen.findByText('item1');
     
-    // Vérifie que l'élément "item1" est rendu, cela confirme que "data" est un objet JSON
     expect(dataElement).toBeInTheDocument();
   });
 });
